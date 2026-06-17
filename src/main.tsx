30cbc0bb4191ec6341f6f32ad8e444b7ed249be7
Szymon <szymon.osinski.2009@gmail.com>
2026-06-17 12:45:27 +0200

    chore: folder structure

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
-import { router } from "./router/AppRouter.tsx";
+import { router } from "./app/router/AppRouter.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
