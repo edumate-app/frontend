60acf14d7abab1909987e0329f26c1184b5c69c9
Szymon <szymon.osinski.2009@gmail.com>
2026-06-20 13:35:39 +0200

    feat: setup auth hooks; useAuthStatus, useLogin, useLogout, useRegister; add auth.store with use of zustand

---
+++ src/features/auth/hooks/useAuthStatus.ts
@@ -0,0 +1,25 @@
+import { useEffect } from "react";
+import { authApi } from "../api/auth.api";
+import { useAuthStore } from "../store/auth.store";
+
+export const useAuthStatus = () => {
+  const setAuth = useAuthStore((state) => state.setAuth);
+  const setAuthChecked = useAuthStore((state) => state.setAuthChecked);
+  const hasCheckedAuth = useAuthStore((state) => state.hasCheckedAuth);
+  useEffect(() => {
+    if (hasCheckedAuth) return;
+    authApi
+      .me()
+      .then(({ data }) => {
+        setAuth({
+          id: data.id,
+          email: data.email,
+          name: data.name,
+          profileUrl: data.profileUrl,
+        });
+      })
+      .finally(() => {
+        setAuthChecked();
+      });
+  }, [hasCheckedAuth, setAuth, setAuthChecked]);
+};
---
+++ src/features/auth/hooks/useLogin.ts
@@ -0,0 +1,21 @@
+import { authApi } from "../api/auth.api";
+import { useAuthStore } from "../store/auth.store";
+
+export const useLogin = () => {
+  const setAuth = useAuthStore((s) => s.setAuth);
+
+  const login = async (email: string, password: string) => {
+    await authApi.login({ email, password });
+    await authApi.me().then((response) => {
+      const user = response.data;
+      setAuth({
+        id: user.id,
+        email: user.email,
+        name: user.name,
+        profileUrl: user.profileUrl,
+      });
+    });
+  };
+
+  return { login };
+};
